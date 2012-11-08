27c14feffa96a3ae28d760572b2b303e3aeb1ebd
haint <haithanh0809@gmail.com>
2012-11-08 17:40:10 +0700

    Finish

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,12 +3,20 @@
 # Update sources
 # apt-get update
 
-# Install Apache Tomcat and GIT
-apt-get install -y openjdk-6-jre-headless
-apt-get install -y git-core
+# Install Tomcat and GIT
+apt-get install -y tomcat6 tomcat6-admin tomcat6-user git-core git-daemon-run
+
+# Configure git daemon
+sv stop git-daemon
+sed -i 's/\/var\/cache\ \/var\/cache\/git/\/home\/vagrant/g' /etc/sv/git-daemon/run
+sv start git-daemon
+
 mkdir repository.git
 cd repository.git
 git --bare init
+touch git-daemon-export-ok
+cd .. && chown vagrant:vagrant -R repository.git
 
-# Change owner for tomcat
-#chown -R vagrant:vagrant /var/lib/tomcat6
+# Setup and configure Tomcat
+echo -e "<tomcat-users>\n<role rolename=\"manager\"/>\n<user username=\"tomcat\" password=\"tomcat\" roles=\"manager\"/>\n</tomcat-users>" > "/etc/tomcat6/tomcat-users.xml"
+service tomcat6 restart
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,16 @@
 
 # Environment setting
 VAGRANT_HOME="/home/haint/java/vagrant"
-SERVER="192.168.33.10"
+#SERVER="192.168.33.10"
 VAGRANT_DELIVERY="$VAGRANT_HOME/delivery"
 # End setting
 
-# Cleanup delivery directory
+# Cleanup delivery and logs directories
 rm "$VAGRANT_DELIVERY" -rf
+rm "$VAGRANT_HOME/logs" -rf
 
-git clone "vagrant@$SERVER:repository.git" "$VAGRANT_DELIVERY"
+git clone "ssh://vagrant@localhost:2222/home/vagrant/repository.git" "$VAGRANT_DELIVERY"
+mkdir "$VAGRANT_HOME/logs"
 
 cd "$VAGRANT_DELIVERY"
 branch=`git branch`
@@ -23,9 +25,67 @@ fi
 
 cd $OLDPWD
 
+process()
+{
+	for e in $1; do
+		if [[ "$e" == *manifest.txt ]]; then
+			echo "detected manifest file: $e"
+			manifest=$e
+			while read line
+			do
+				if [[ "$line" == \#* ]]; then
+					continue
+				else
+					IFS=',' read -a segments <<< "$line"
+
+					#Begin commit
+					branch=${segments[1]}
+					echo "branch: $branch"
+					git checkout -b "$branch"
+
+					filename=${segments[0]}
+					echo "file name: $filename"
+					git add "$filename"
+
+					comment=${segments[2]}
+					echo "comment: $comment"
+					git commit -m "$comment"
+
+					CMD=`git push origin "$branch" 2>&1`
+					if [ $? != 0 ]; then
+						error="[ERROR][`date`] Commit file $filename to branch $branch with commnet $comment occurs an error: \n $CMD \n"
+						echo -e $error >> ../logs/delivery.txt
+						echo -e $error
+					else
+						echo "[INFO]$CMD"
+					fi
+					#End commit
+
+					#Begin deploy war
+					if [[ "$filename" == *.war ]]; then
+						appname=${filename%.*}
+						echo "appName=$appname"
+						CMD=`curl --upload-file "$filename" "http://tomcat:tomcat@localhost:8085/manager/deploy?path=/$branch-${filename%.*}&update=true"`
+						if [ $? != 0 ]; then
+							error="[ERROR][`date`] Deploy $filename to context path /$branch-${filename%.*} occurs an error: \n $CMD \n"
+							echo -e $error >> ../logs/deployment.txt
+							echo -e $error
+						else
+							info="[INFO][`date`]\n $CMD \n"
+							echo -e $info >> ../logs/deployment.txt
+							echo -e $info
+						fi
+					fi
+					#End deploy war
+				fi
+			done < "$manifest"
+		fi
+	done
+}
+
 current="0"
 
-echo "Listening ...."
+echo "Started"
 while true; do
 	list=`find "$VAGRANT_HOME/delivery" -name "*.tar.gz" -o -name "*.zip"`
 	length="0"
@@ -39,41 +99,8 @@ while true; do
 				echo "detected tar file: $item"
 				cd "$VAGRANT_DELIVERY"
 				elements=`tar xvzf $item`
-				for e in $elements; do
-					if [[ "$e" == *manifest.txt ]]; then
-						echo "detected manifest file: $e"
-						manifest=$e
-						while read line
-						do
-							if [[ "$line" == \#* ]]; then
-								continue
-							else
-								IFS=',' read -a segments <<< "$line"
-								#for index in ${!segments[@]}
-								#do
-								#	echo "$index ${segments[index]}"
-								#done
-								branch=${segments[1]}
-								echo "branch: $branch"
-								git checkout -b "$branch"
-
-								filename=${segments[0]}
-								echo "file name: $filename"
-								git add "$filename"
-
-								comment=${segments[2]}
-								echo "comment: $comment"
-								git commit -m "$comment"
-
-								CMD=`git push origin "$branch" 2>&1`
-								if [ $? != 0 ]; then
-									echo "Error"
-									#echo $CMD
-								fi
-							fi
-						done < "$manifest"
-					fi
-				done
+				rm $item
+				process "$elements"
 			elif [[ "$item" == *.zip ]]; then
 				echo "detected zip file: $item"
 			fi
@@ -82,3 +109,4 @@ while true; do
 	fi
 	current=$length
 done
+
